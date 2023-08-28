533bfbd02b41c2dcb3420a3fc517aa5973820d10
sierra182 <sierra-echo@msn.com>
2023-08-28 16:14:41 +0200

    add recursive.c

---
+++ recursion.c
@@ -0,0 +1,26 @@
+#include "headerfile.h"
+
+typedef struct
+{
+
+} s_solution;
+
+int     ft_rec_search_optimal_solution(s_solution sol)
+{
+    int     i;
+    int     direction;
+
+    i = 0;
+    direction = 0;
+    while (i < n_tetra)
+    {
+        while (direction < 2)
+        {
+            sol[][][]
+            ft_rec_search_optimal_solution(s_solution sol);
+            direction++;
+        }
+        direction = 0;
+        i++;
+    }
+}    
