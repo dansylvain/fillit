b3135a8c45e63676dce9431ed0016cd04634f003
dansylvain <dansylvain84@gmail.comgit config --global user.email dansylvain84@gmail.com>
2023-08-28 11:54:53 +0200

    welcome to the gros bordel

--- get_input.c
+++ get_input.c
@@ -2,51 +2,37 @@
 
 int    check_input(Data *data, char *c)
 {
-    int i;
     int j;
     int k;
     int l;
 
-    i = 0;  // carac counter (string level)
     j = 0;  // letter counter (tetriminos level)
     k = 0;  // line counter
     l = 0;  // tetriminos counter
-    while (i < data->input_size)
+    while (*c)
     {
-        if (c[i] != '\n')
-            j++;
-        if ((c[i] == '\n' || i == data->input_size - 1) && j != 4 && c[i - 1] != '\n')
-        {
-            display_error();
+        if (*c != '.' && *c != '#' && *c != '\n')
             return (0);
-        }
-        if (c[i] == '\n')
+        if (*c != '\n')
+            j++;
+        if ((*c == '\n' || *(c + 1) == '\0') && j != 4 && *(c - 1) != '\n')
+            return (0);
+        if (*c == '\n')
         {
             k++;
             j = 0;
         }
-        if (c[i] == '\n' && c[i - 1] == '\n' && k != 5)
-            {
-                display_error();
+        if (*c == '\n' && *(c -1) == '\n' && k != 5)
                 return (0);
-            }
-        if (c[i]  == '\n' && c[i - 1] == '\n')
+        if (*c  == '\n' && *(c - 1) == '\n')
         {
             k = 0;
             l++;
         }
-        if (c[i] != '.' && c[i] != '#' && c[i] != '\n')
-        {
-                display_error();    
-                return (0);
-        }
-        i++;
+        c++;
     }
     if (l == 0 || l > 25)
-    {
-        display_error();
         return (0);
-    }
     data->tetnum = l + 1;
     return (1);
 }
